e030fddc71d7c17298a5143b7bce03c4530c1cf0
Pika-Kun 3 <121213527+PikaBotz@users.noreply.github.com>
2023-11-30 00:19:12 +0530

    Update imgtotext.js

--- api/imgtotext.js
+++ api/imgtotext.js
@@ -3,15 +3,18 @@ const { isKey } = require('../func/checkkey.js');
 const code = require('../func/codes.js');
 
 export default async (req, res) => {
-  const key = req.query.key || false;
-  const media = req.body.media || false;
+  try {
+    const key = req.query.key || false;
+    const media = req.body && req.body.media ? req.body.media : false;
 
-  if (!isKey(key, res)) return;
+    if (!isKey(key, res) || !media) {
+      throw new Error('Invalid key or missing media');
+    }
 
-  totext.imgToText(Buffer.from(media, 'base64'), 'eng')
-    .then((result) => code._200(res, { text: result }))
-    .catch((error) => {
-      console.error(error);
-      code._403(res);
-    });
+    const textResult = await totext.imgToText(Buffer.from(media, 'base64'), 'eng');
+    code._200(res, { text: textResult });
+  } catch (error) {
+    console.error(error);
+    code._403(res);
+  }
 };
