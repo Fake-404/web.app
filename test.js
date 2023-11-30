4a7e5c8d13e9090990418f429a9d2fcfd95a8935
Pika-Kun 3 <121213527+PikaBotz@users.noreply.github.com>
2023-11-30 05:22:44 +0000

    Pending changes exported from your codespace

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 /* Only for testing */
+
+const fs = require('fs')
+const image = fs.readFileSync('./test.jpg')
+
+const { imgToText } = require('./lib/scraper')
+
+imgToText(image, 'eng')
+.then((res) => console.log(res))
