02cd11c5fdbf0f8820067a5d7e27d058dd761ceb
Pika-Kun 3 <121213527+PikaBotz@users.noreply.github.com>
2023-12-01 23:37:02 +0530

    Update imgtotext.js

--- api/imgtotext.js
+++ api/imgtotext.js
@@ -1,15 +1,19 @@
-const imgToText = require('../lib/scraper');
+const js = require('../lib/scraper');
 const { isKey } = require('../func/checkkey.js');
 const code = require('../func/codes.js');
 
 export default async (req, res) => {
   const key = req.query.key || false;
-  const media = req.body.media || false;
+  const code = req.body.code || false;
 
   if (!isKey(key, res)) return;
-
-  imgToText.imgToText(media, 'eng')
-    .then((result) => code._200(res, { text: result }))
+  if (!code) {
+    code._404(res, "Enter a javascript");
+    return;
+  }
+  
+  js.obfuscateJs(code)
+    .then((result) => code._200(res, result))
     .catch((error) => {
       console.error(error);
       code._403(res);
