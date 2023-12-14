d510ada38a107236a291eeb16e1f4ba8fa3af16f
Fake-404 <120786742+Fake-404@users.noreply.github.com>
2023-12-14 14:27:23 +0530

    Delete JS directory

--- JS/api.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('translateButton').addEventListener('click', function () {
- window.location.href = `https://pikaapi.vercel.app/api/translate.js?key=accessKey&fl=auto&tl=en&q=to%20kese%20hai%20aap%20log`;
-});
-
-document.getElementById("ytvidButton").addEventListener("click", function() {
- window.location.href = `https://pikaapi.vercel.app/api/ytvid.js?key=accessKey&url=https://youtu.be/jEwjrdzrpWE?si=k3iaCyElIt30bbz7`;
-});
